4336e702e7d6fb419b77747c742423ff6819ca79
Mason.Lin <mason.lin@vivotek.com>
2020-01-30 17:22:00 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 import setuptools
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mason_image',
     version='0.1.0',
-    description='mess up a image',
     author='Mason Lin',
     author_email='pizza0117@gmail.com',
-    url='https://github.com/Mason-Lin',
+    description='mess up a image',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Mason-Lin/mason_image',
     packages=setuptools.find_packages(),
-    install_requires=['Pillow>=5.1.0', 'numpy==1.14.4'],
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+    install_requires=['Pillow>=5.1.0'],
     entry_points={
         'console_scripts': [
             'mason_image=mason_image:main'
         ],
     },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
